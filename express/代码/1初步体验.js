e28daad8231412825af3b49ce8055cb579480abe
liujiansen <1179973857@qq.com>
2025-06-26 23:20:15 +0800

    初步体验express

---
+++ express/代码/1初步体验.js
@@ -0,0 +1,15 @@
+//1 导入express
+const express = require('express');
+
+//2.创建对象
+const app = express();
+
+//3.创建路由
+app.get('/home',(req,res)=>{
+    res.end('hello express')
+})
+
+//4.监听端口号
+ app.listen(3000,()=>{
+    console.log('服务已启动')
+ })
